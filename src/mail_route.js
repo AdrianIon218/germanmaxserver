7d9a91445bf555123e7d009f574b75b426cadcc3
AdrianIon218 <ionadrian218@yahoo.com>
2024-01-22 16:34:30 +0200

    decreased the time to delete the code and changed the condition based on code as well

--- src/mail_route.js
+++ src/mail_route.js
@@ -3,15 +3,15 @@ const router = express.Router();
 const supabase = require("../supabase");
 const sendMailWithCode = require("./send_mail");
 
-const addTimerToDeleteTheCode = (email) => {
+const addTimerToDeleteTheCode = (email, code) => {
    setTimeout(
      async () => {
-      const { error } = await supabase.from('expiring_code').delete().eq('email', email);
+      const { error } = await supabase.from('expiring_code').delete().eq('email', email).eq("code", code);
       if(error){
          console.error(error,`\nCould not delete the expiring code for email: ${email}`);
        }
      },
-     12 * 60 * 1000,
+     11 * 60 * 1000,
    );
  };
 
@@ -19,18 +19,18 @@ router.post("/", async (req, res) => {
    const failReq = ()=>{
       res.json({status:"failed"});
    }
-   const successReq = (email)=>{
+   const successReq = (email, code)=>{
       const expire_at = new Date();
       expire_at.setMinutes(expire_at.getMinutes() + 10);
       const expire_date = expire_at.toLocaleTimeString().replace("AM", "").replace("PM", "").trim();
-      addTimerToDeleteTheCode(email);
+      addTimerToDeleteTheCode(email, code);
       res.json({status:"ok", dateToExpire: expire_date});
    }
    const { email, code } = req.body;
    const mailObj = { to:email, code, failReq, successReq };
 
    try{
-     await sendMailWithCode(mailObj);
+     await sendMailWithCode(mailObj, code);
    }
    catch(err){
      failReq();
